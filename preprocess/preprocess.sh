af727c6a6b5570463a265a746d675bb0fb863f0f
liuqiao <liuqiao@g01.openness>
2019-03-26 23:42:48 +0800

    update scripts instructions

--- preprocess/preprocess.sh
+++ preprocess/preprocess.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-DPATH=$1
-CELL=$2
+PPATH=$(dirname $(readlink -f "$0"))
+DPATH=${PPATH/preprocess/data}
+CELL=$1
 mkdir -p "$DPATH/$CELL"
 #merge raw data 
 find "$DPATH/$CELL" -name "*_merged_nodups.txt.gz"|xargs zcat | sort -k3,3d -k7,7d > "$DPATH/$CELL/total_merged_nodups.txt"
@@ -15,8 +16,9 @@ mkdir -p "$DPATH/$CELL/intra_NONE"
 mkdir -p "$DPATH/$CELL/intra_VC"
 mkdir -p "$DPATH/$CELL/intra_KR"
 
+#write your own path to juicer tools here
 juicer_tool="/home/liuqiao/software/juicer/scripts/juicer_tools.jar"
-#generate .HIC file using juicer tool
+#generate .HIC file using juicer tool, -xmx50g indicates 50g for memory which can be replaced with an appropriate value
 java -Xmx50g  -jar $juicer_tool pre $DPATH/$CELL/total_merged_nodups.txt $DPATH/$CELL/total_merged.hic hg19
 java -Xmx50g  -jar $juicer_tool pre $DPATH/$CELL/total_merged_nodups_downsample_ratio_$ratio.txt $DPATH/$CELL/total_merged_downsample_ratio_$ratio.hic hg19
 
@@ -25,6 +27,6 @@ chromes=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 "X" "Y")
 #generate Hi-C raw contacts using iuicer tool
 for chrom in ${chromes[@]}
 do
-	sbatch sbatch_juicer_script.sh $chrom $DPATH $CELL
+	sbatch $PPATH/sbatch_juicer_script.sh $chrom $DPATH $CELL
 	#directly run "bash sbatch_juicer_script.sh $chrom $DPATH $CELL" if slurm is not installed
 done
--- preprocess/raw_data_download_script.sh
+++ preprocess/raw_data_download_script.sh
@@ -1,16 +1,47 @@
 #!/bin/bash
 
-#Cell type indexes are as follows:
+#Cell type indexes are as follows (refer to GSE63525):
 #GM12878 idx: 1-18
 #K562 idx:69-74
 #IMR90 idx:50-56
 #NHEK idx:65-67
 #we take GM12878 cell type for an example
+declare -i start_idx
+declare -i end_idx
+declare -i idx
 
-for idx in {1..18}
+PPATH=$(dirname $(readlink -f "$0"))
+DPATH=${PPATH/preprocess/data}
+mkdir -p "$DPATH/$1" 
+	
+if [ $1 == 'GM12878' ]
+then
+	start_idx=1
+	end_idx=18
+elif [ $1 == 'K562' ]
+then
+	start_idx=69
+	end_idx=74
+elif [ $1 == 'IMR90' ]
+then
+	start_idx=50
+	end_idx=56
+elif [ $1 == 'NHEK' ]
+then
+	start_idx=65
+	end_idx=67
+else
+	echo "The input cell type is not identified, please refer to GEO database with accession number GSE63525"
+	exit
+fi
+
+for ((idx=$start_idx; idx<=$end_idx; idx++))
 do
 	number=$(expr $idx + 549)
-	folder_idx=`printf "%03d" $number`
-	#echo $folder_idx
-	wget ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM1551nnn/GSM1551${folder_idx}/suppl/GSM1551${folder_idx}_HIC$1_merged_nodups.txt.gz
+	wget -P "$DPATH/$1" ftp://ftp.ncbi.nlm.nih.gov/geo/samples/GSM1551nnn/GSM1551`printf "%03d" $number`/suppl/GSM1551`printf "%03d" $number`_HIC`printf "%03d" $idx`_merged_nodups.txt.gz
 done
+
+
+
+
+
