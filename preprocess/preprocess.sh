581a7c2eca138f4e969dcdd1501e6bfbd8fb8f2b
junjiechen <499690505@qq.com>
2021-07-03 11:15:30 +0800

    fix variant names

--- preprocess/preprocess.sh
+++ preprocess/preprocess.sh
@@ -19,10 +19,10 @@ mkdir -p "$DPATH/$CELL/intra_VC"
 mkdir -p "$DPATH/$CELL/intra_KR"
 
 #write your own path to juicer tools here
-juicer_tool="/home/liuqiao/software/juicer/scripts/juicer_tools.jar"
+# juicer_tool="/home/liuqiao/software/juicer/scripts/juicer_tools.jar"
 #generate .HIC file using juicer tool, -xmx50g indicates 50g for memory which can be replaced with an appropriate value
-java -Xmx50g  -jar $juicer_tool pre $DPATH/$CELL/total_merged_nodups.txt $DPATH/$CELL/total_merged.hic hg19
-java -Xmx50g  -jar $juicer_tool pre $DPATH/$CELL/total_merged_nodups_downsample_ratio_$ratio.txt $DPATH/$CELL/total_merged_downsample_ratio_$ratio.hic hg19
+java -Xmx50g  -jar $juicer_path pre $DPATH/$CELL/total_merged_nodups.txt $DPATH/$CELL/total_merged.hic hg19
+java -Xmx50g  -jar $juicer_path pre $DPATH/$CELL/total_merged_nodups_downsample_ratio_$ratio.txt $DPATH/$CELL/total_merged_downsample_ratio_$ratio.hic hg19
 
 chromes=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 "X" "Y")
 
--- preprocess/sbatch_juicer_script.sh
+++ preprocess/sbatch_juicer_script.sh
@@ -14,11 +14,12 @@ DPATH=$2
 CELL=$3
 resolution=$4
 juicer_tool=$5
-java -jar $juicer_tool dump observed NONE $DPATH/$2/total_merged.hic $1 $1 BP $resolution $DPATH/$2/intra_NONE/chr$1_10k_intra_NONE.txt 
-java -jar $juicer_tool dump observed VC $DPATH/$2/total_merged.hic $1 $1 BP $resolution $DPATH/$2/intra_VC/chr$1_10k_intra_VC.txt 
-java -jar $juicer_tool dump observed KR $DPATH/$2/total_merged.hic $1 $1 BP $resolution $DPATH/$2/intra_KR/chr$1_10k_intra_KR.txt 
+# $(($a + $b)) is expression;   $a\_ is the $a without "_"
+java -jar $juicer_tool dump observed NONE $DPATH/$CELL/total_merged.hic $chrom $chrom BP $resolution $DPATH/$CELL/intra_NONE/chr$chrom\_res$(($resolution/1000))k_intra_NONE.txt 
+java -jar $juicer_tool dump observed VC $DPATH/$CELL/total_merged.hic $chrom $chrom BP $resolution $DPATH/$CELL/intra_VC/chr$chrom\_res$(($resolution/1000))k_intra_VC.txt 
+java -jar $juicer_tool dump observed KR $DPATH/$CELL/total_merged.hic $chrom $chrom BP $resolution $DPATH/$CELL/intra_KR/chr$chrom\_res$(($resolution/1000))k_intra_KR.txt 
 
-java -jar $juicer_tool dump observed NONE $DPATH/$2/total_merged_downsample_ratio_$ratio.hic $1 $1 BP $resolution $DPATH/$2/intra_NONE/chr$1_10k_intra_NONE_downsample_ratio$ratio.txt
-java -jar $juicer_tool dump observed VC $DPATH/$2/total_merged_downsample_ratio_$ratio.hic $1 $1 BP $resolution $DPATH/$2/intra_VC/chr$1_10k_intra_VC_downsample_ratio$ratio.txt
-java -jar $juicer_tool dump observed KR $DPATH/$2/total_merged_downsample_ratio_$ratio.hic $1 $1 BP $resolution $DPATH/$2/intra_KR/chr$1_10k_intra_KR_downsample_ratio$ratio.txt
+java -jar $juicer_tool dump observed NONE $DPATH/$CELL/total_merged_downsample_ratio_$ratio.hic $chrom $chrom BP $resolution $DPATH/$CELL/intra_NONE/chr$chrom\_res$(($resolution/1000))k_intra_NONE_downsample_ratio$ratio.txt
+java -jar $juicer_tool dump observed VC $DPATH/$CELL/total_merged_downsample_ratio_$ratio.hic $chrom $chrom BP $resolution $DPATH/$CELL/intra_VC/chr$chrom\_res$(($resolution/1000))k_intra_VC_downsample_ratio$ratio.txt
+java -jar $juicer_tool dump observed KR $DPATH/$CELL/total_merged_downsample_ratio_$ratio.hic $chrom $chrom BP $resolution $DPATH/$CELL/intra_KR/chr$chrom\_res$(($resolution/1000))k_intra_KR_downsample_ratio$ratio.txt
 
